92706983f3300cfa278c3a35d2c2961f03deded6
Wilbert Navarrete <wilmion92@gmail.com>
2021-03-27 06:57:50 +0100

    test fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Authorization');
     next();
 })
 router(app);
